fee585cf7845cb3493ec63716139f0a201cd2a21
bhumipandey06 <bhumipandey1188@gmail.com>
2025-08-13 23:01:19 +0530

    implemented background image

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import assets from "../assets/assets"
 
 const Sidebar = ({selectedUser, setSelectedUser}) => {
   return (
-    <div>Sidebar</div>
+    <div className='pb-5'>
+        <div className='flex justify-between items-center'>
+            <img src={assets.logo} alt="" />
+        </div>
+    </div>
   )
 }
 

b06f21009ce347a1c28399fcbe0ae9cd8d1d6e36
bhumipandey06 <bhumipandey1188@gmail.com>
2025-08-18 22:28:53 +0530

    updated home page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
 import RightSidebar from '../components/RightSidebar'
 
-const HomePage = () => {
+const HomePage =()=> {
     const [selectedUser, setSelectedUser] = useState(false)
   return (
     <div className=' border w-full h-screen sm:px-[15%] sm:py-[5%]'>
